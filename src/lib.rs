90bff86ba3335de74d75833c85d9c7019a510a2b
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2021-11-30 20:19:25 +0100

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
-use crate::lexer::Lexer;
+use crate::lexer::tokens::Token;
+use crate::lexer::{Lexer, LexerError};
 
 mod error;
 mod lexer;
 mod parse;
+
+pub use parse::parse;
 
 /// this is mainly to remove the dead code warnings on code that is not actually dead
 pub fn run(code: &str) {
@@ -11,4 +14,11 @@ pub fn run(code: &str) {
     if let Ok(tokens) = tokens {
         let _ = parse::parse(tokens.into_iter());
     }
+}
+
+pub fn lex(code: &str) -> Result<Vec<Token>, LexerError> {
+    let lower = code.to_lowercase();
+    let mut lexer = Lexer::new(&lower);
+
+    lexer.compute_tokens()
 }
