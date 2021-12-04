53909ac406585df98c4d935f247f62b8bde90579
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2021-12-04 14:07:36 +0100

    fix fuzz

--- fuzz/fuzz_targets/lex_parse.rs
+++ fuzz/fuzz_targets/lex_parse.rs
@@ -4,7 +4,7 @@ use libfuzzer_sys::fuzz_target;
 fuzz_target!(|data: &[u8]| {
     if let Ok(input) = std::str::from_utf8(data) {
         if let Ok(tokens) = vfpl::lex(&input) {
-            let _ = vfpl::parse(tokens);
+            let _ = vfpl::parse(tokens.into_iter());
         }
     }
 });
