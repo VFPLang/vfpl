e6f7b0ff89f2cbe65fc43c5e62aaaae84979ff98
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2021-12-04 19:42:00 +0100

    add ident to literal in interpreter

--- src/interpret_ast/exec.rs
+++ src/interpret_ast/exec.rs
@@ -1,7 +1,6 @@
 use crate::error::Span;
 use crate::interpret_ast::{
-    Env, EvalCallArg, FnImpl, IResult, Ident, InterpreterError, Interrupt, RuntimeFn, Value,
-    ValueResult, Vm,
+    Env, FnImpl, IResult, Ident, InterpreterError, Interrupt, RuntimeFn, Value, ValueResult, Vm,
 };
 use crate::parse::ast::{
     ArithmeticOp, Call, Comparison, ComparisonKind, Else, ElseKind, Expr, FnDecl, IfPart, Literal,
@@ -49,24 +48,32 @@ impl Vm {
 
     fn eval(&mut self, expr: &Expr) -> ValueResult {
         match expr {
-            Expr::Literal(lit) => Ok(self.eval_literal(lit)),
+            Expr::Literal(lit) => self.eval_literal(lit),
             Expr::Call(call) => self.eval_call(call),
             Expr::Comparison(comp) => self.eval_comparison(comp),
         }
     }
 
-    fn eval_literal(&self, lit: &Literal) -> Value {
+    fn eval_literal(&mut self, lit: &Literal) -> ValueResult {
         match &lit.kind {
-            LiteralKind::Absent => Value::Absent,
-            LiteralKind::Null => Value::Null,
-            LiteralKind::NoValue => Value::NoValue,
-            LiteralKind::Undefined => Value::Undefined,
-            LiteralKind::String(string) => Value::String(string.clone().into()),
-            LiteralKind::Int(int) => Value::Int(*int),
-            LiteralKind::Float(float) => Value::Float(*float),
-            LiteralKind::True => Value::Bool(true),
-            LiteralKind::False => Value::Bool(false),
-            LiteralKind::Ident(_) => todo!(),
+            LiteralKind::Absent => Ok(Value::Absent),
+            LiteralKind::Null => Ok(Value::Null),
+            LiteralKind::NoValue => Ok(Value::NoValue),
+            LiteralKind::Undefined => Ok(Value::Undefined),
+            LiteralKind::String(string) => Ok(Value::String(string.clone().into())),
+            LiteralKind::Int(int) => Ok(Value::Int(*int)),
+            LiteralKind::Float(float) => Ok(Value::Float(*float)),
+            LiteralKind::True => Ok(Value::Bool(true)),
+            LiteralKind::False => Ok(Value::Bool(false)),
+            LiteralKind::Ident(name) => {
+                self.env().get_value(name.clone().into()).ok_or_else(|| {
+                    InterpreterError {
+                        span: lit.span,
+                        message: format!("Variable not found: {}", name),
+                    }
+                    .into()
+                })
+            }
         }
     }
 
@@ -115,6 +122,15 @@ impl Vm {
             .into());
         }
 
+        // this is only needed here as a convenience struct
+
+        #[derive(Debug)]
+        struct EvalCallArg {
+            value: Value,
+            span: Span,
+            name: Ident,
+        }
+
         let arg_values = args
             .iter()
             .map(|arg| {
@@ -127,7 +143,6 @@ impl Vm {
             .collect::<Result<Vec<_>, Interrupt>>()?;
 
         // do param type and name checking
-
         params
             .iter()
             .zip(arg_values.iter())
