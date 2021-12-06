9a9d2ba86fbad5097a23c8a15e859a23fc3220db
nils <48135649+Nilstrieb@users.noreply.github.com>
2021-12-06 15:36:33 +0100

    fully migrate error + global

---
+++ vfpl_ast/src/lib.rs
@@ -0,0 +1,268 @@
+use std::fmt::{Display, Formatter, Write};
+use vfpl_error::Span;
+
+type Ident = String;
+
+pub type Program = Body;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Body {
+    pub span: Span,
+    pub stmts: Vec<Stmt>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct TypedIdent {
+    pub span: Span,
+    pub name: Ident,
+    pub ty: Ty,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Stmt {
+    VarInit(VarInit),
+    VarSet(VarSet),
+    If(If),
+    While(While),
+    FnDecl(FnDecl),
+    Break(Break),
+    Return(Return),
+    Terminate(Terminate),
+    Expr(Expr),
+}
+
+impl Stmt {
+    pub fn span(&self) -> Span {
+        match self {
+            Stmt::VarInit(inner) => inner.span,
+            Stmt::VarSet(inner) => inner.span,
+            Stmt::If(inner) => inner.span,
+            Stmt::While(inner) => inner.span,
+            Stmt::FnDecl(inner) => inner.span,
+            Stmt::Break(inner) => inner.span,
+            Stmt::Return(inner) => inner.span,
+            Stmt::Terminate(inner) => inner.span,
+            Stmt::Expr(expr) => expr.span(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct VarInit {
+    pub span: Span,
+    pub name: TypedIdent,
+    pub init: Expr,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct VarSet {
+    pub span: Span,
+    pub name: Ident,
+    pub expr: Expr,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct If {
+    pub span: Span,
+    pub if_part: IfPart,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct IfPart {
+    pub span: Span,
+    pub cond: Box<Expr>,
+    pub body: Body,
+    pub else_part: Option<Else>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Else {
+    pub span: Span,
+    pub kind: ElseKind,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ElseKind {
+    ElseIf(Box<IfPart>),
+    Else(Body),
+}
+
+impl ElseKind {
+    pub fn span(&self) -> Span {
+        match self {
+            ElseKind::Else(body) => body.span,
+            ElseKind::ElseIf(if_) => if_.span,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct While {
+    pub span: Span,
+    pub cond: Expr,
+    pub body: Body,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Break {
+    pub span: Span,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct FnDecl {
+    pub span: Span,
+    pub name: Ident,
+    pub params: FnParams,
+    pub fn_return: FnReturn,
+    pub body: Body,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct FnParams {
+    pub span: Span,
+    pub params: Vec<TypedIdent>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct FnReturn {
+    pub span: Span,
+    pub ty: Ty,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Return {
+    pub span: Span,
+    pub expr: Expr,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Terminate {
+    pub span: Span,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Ty {
+    pub span: Span,
+    pub kind: TyKind,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum TyKind {
+    Any,
+    Name(Ident),
+    Integer,
+    Float,
+    Boolean,
+    String,
+    Absent,
+    Null,
+    NoValue,
+    Undefined,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Expr {
+    Literal(Literal),
+    Comparison(Comparison),
+    ArithmeticOp(ArithmeticOp),
+    Call(Call),
+}
+
+impl Expr {
+    pub fn span(&self) -> Span {
+        match self {
+            Expr::Literal(lit) => lit.span,
+            Expr::Comparison(comp) => comp.span,
+            Expr::Call(call) => call.span,
+            Expr::ArithmeticOp(op) => op.span,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Literal {
+    pub span: Span,
+    pub kind: LiteralKind,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum LiteralKind {
+    Absent,
+    Null,
+    NoValue,
+    Undefined,
+    String(String),
+    Int(i64),
+    Float(f64),
+    True,
+    False,
+    Ident(Ident),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Comparison {
+    pub span: Span,
+    pub lhs: Box<Expr>,
+    pub rhs: Box<Expr>,
+    pub kind: ComparisonKind,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ArithmeticOp {
+    pub span: Span,
+    pub lhs: Box<Expr>,
+    pub rhs: Box<Expr>,
+    pub kind: ArithmeticOpKind,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ArithmeticOpKind {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ComparisonKind {
+    NotEq,
+    Eq,
+    Greater,
+    Less,
+    GreaterEq,
+    LessEq,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Call {
+    pub span: Span,
+    pub fn_name: Ident,
+    pub args: CallArgs,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct CallArgs {
+    pub span: Span,
+    pub args: Vec<CallArg>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct CallArg {
+    pub span: Span,
+    pub expr: Expr,
+    pub name: Ident,
+}
+
+impl Display for ComparisonKind {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ComparisonKind::NotEq => f.write_str("!="),
+            ComparisonKind::Eq => f.write_str("=="),
+            ComparisonKind::Greater => f.write_char('>'),
+            ComparisonKind::Less => f.write_char('<'),
+            ComparisonKind::GreaterEq => f.write_str(">="),
+            ComparisonKind::LessEq => f.write_str("<="),
+        }
+    }
+}
---
+++ vfpl_error/src/span.rs
@@ -0,0 +1,82 @@
+///
+/// A Span points to a location in the source code
+///
+/// The start is inclusive, the end is exclusive
+///
+/// The special state, where both start and end are [usize::MAX] stands for the EOF span
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Default)]
+pub struct Span {
+    pub start: usize,
+    pub end: usize,
+}
+
+impl Span {
+    /// Create a span with a start and length
+    pub fn start_len(start: usize, len: usize) -> Self {
+        Self {
+            start,
+            end: start + len,
+        }
+    }
+
+    /// Create a span with a start and end
+    pub fn start_end(start: usize, end: usize) -> Self {
+        Self { start, end }
+    }
+
+    /// Create a span for a single character starting at `start`
+    pub fn single(start: usize) -> Self {
+        Self {
+            start,
+            end: start + 1,
+        }
+    }
+
+    /// Create an empty span
+    pub fn dummy() -> Self {
+        Self { start: 0, end: 0 }
+    }
+
+    pub fn eof() -> Self {
+        Self {
+            start: usize::MAX,
+            end: usize::MAX,
+        }
+    }
+
+    /// Checks whether the Span is the dummy span
+    pub fn is_dummy(&self) -> bool {
+        *self == Span::dummy()
+    }
+
+    /// Extends the span by the second one
+    /// The other one has to be after the current one
+    pub fn extend(&self, other: Span) -> Span {
+        if other.is_dummy() {
+            return *self;
+        }
+        if self.is_dummy() {
+            return other;
+        }
+
+        debug_assert!(self.start <= other.start);
+        debug_assert!(self.end <= other.end);
+        Span {
+            start: self.start,
+            end: other.end,
+        }
+    }
+
+    /// Extends the span by the second one, if it exists
+    /// The other one has to be after the current one, if it exists
+    pub fn option_extend(&self, other: Option<Span>) -> Span {
+        match other {
+            None => *self,
+            Some(span) => self.extend(span),
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.end - self.start
+    }
+}
